37726453963a7bf262b39405b97bd62672f0b0cc
thiago-silva-santos <thiagomj98@gmail.com>
2022-11-12 10:51:22 -0300

    Componentes e deploy

---
+++ components/Header.js
@@ -0,0 +1,40 @@
+import config from '../config.json'
+import styled from 'styled-components'
+function Header() {
+     const StyledHeader = styled.div`
+          img {
+               width: 80px;
+               height: 80px;
+               border-radius: 50%
+          }
+          .user-info {
+               display: flex;
+               align-items: center;
+               width: 100%;
+               padding: 16px 32px;
+               gap: 10px;
+               margin-top: 60px;
+          }
+     `
+     return (
+          <StyledHeader>
+               {/* <img src='banner'/> */}
+               <section className='user-info'>
+                    <img src={`https://github.com/${config.github}.png`} />
+                    <div>
+                         <h2>
+                              {config.name}
+
+                         </h2>
+                         <p>
+
+                              {config.job}
+                         </p>
+                    </div>
+
+               </section>
+          </StyledHeader>
+     )
+}
+
+export default Header
